a5843654d83512eef88301cf70319a1c00aca960
Juan E. Chemello <juan.chemello@bue.edu.ar>
2021-07-05 19:15:06 -0300

    Creo instancia de flask

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
