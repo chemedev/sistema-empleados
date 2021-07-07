10844a8a6dac0d2ddcc629ed7fb88d68aac95b47
Juan E. Chemello <juan.chemello@bue.edu.ar>
2021-07-07 19:34:16 -0300

    Creamos rutas para cargar empleado

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -12,18 +12,43 @@ app.config['MYSQL_DATABASE_DB'] = 'empleados'
 
 mysql.init_app(app)
 
+
 @app.route('/')
 def index():
-  conn = mysql.connect()
-  cursor = conn.cursor()
+    conn = mysql.connect()
+    cursor = conn.cursor()
 
-  sql = "insert into empleados (nombre, correo, foto) values ('Juan', 'juan@email.com', 'fotodejuan.jpg');"
-  cursor.execute(sql)
+    sql = "SELECT * FROM empleados;"
+    cursor.execute(sql)
 
-  conn.commit()
+    empleados = cursor.fetchall()
 
-  return render_template('empleados/index.html')
+    conn.commit()
+
+    return render_template('empleados/index.html', empleados=empleados)
+
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
+
+
+@app.route('/store', methods=["POST"])
+def store():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
+
+    sql = "INSERT INTO empleados (nombre, correo, foto) values (%s, %s, %s);"
+    datos = (_nombre, _correo, _foto.filename)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
